e4fd75dc4de5dd560965910ef820eea2a1eff253
David Poh <david.zpoh@gmail.com>
2020-03-30 15:31:24 +0800

    Completed first react native app

--- App.js
+++ App.js
@@ -1,33 +1,47 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
+  const addGoalHandler = goalTitle => {
+    // console.log(enteredGoal);
+    setCourseGoals(currentGoals => [...currentGoals, { uid: Math.random().toString(), value: goalTitle }]);
+    setIsAddMode(false);
   };
 
-  const addGoalHandler = () => {
-    // console.log(enteredGoal);
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.uid !== goalId)
+    });
+  };
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
   };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder='Course Goal'
-          style={styles.input}
-          onChangeText={goalInputHandler} 
-          value={enteredGoal}
-          />
-        <Button title="ADD" onPress={addGoalHandler}/>
-      </View>
-      <View>
-        {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
-      </View>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalAdditionHandler}
+      />
+      <FlatList
+        keyExtractor={(item, index) => item.uid} // by default no need as React Native takes id or key as the index key.
+        data={courseGoals}
+        renderItem={itemData => (
+          <GoalItem
+            uid={itemData.item.uid}
+            onDelete={removeGoalHandler}
+            title={itemData.item.value}
+          />)
+        }
+      />
     </View>
   );
 }
@@ -35,22 +49,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%',
-    borderBottomColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderWidth: 1
   }
 });
